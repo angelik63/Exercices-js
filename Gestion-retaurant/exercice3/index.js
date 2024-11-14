0d5075e4aed44782f7846cd35cd72d21df267b4b
angelique <ademon63@hotmail.fr>
2024-11-14 14:51:52 +0100

    gestion-restaurant

---
+++ Gestion-retaurant/exercice3/index.js
@@ -0,0 +1,25 @@
+// const commandes=["Pizza", "Salade", "Pâtes", "Burger", "Dessert"];
+// //  for (let i=0; i<commandes.length; i++){
+// //     console.log(commandes[i]);
+// //  }
+
+// //   let i = 0 
+// //   while (i<commandes.length){
+// //     console.log(commandes[i]);
+// //     if (commandes[i]==="Burger"){
+// //         break;
+// //     }
+// //     i++;
+// //   }
+// let prix;
+// let i =0;
+// do{  
+//     if(commandes[i]==="Burger")
+//     {prix*0.9}
+//     console.log
+// }
+
+const carre =(x)=>{ 
+    return x**2;
+}
+console.log(carre(7));
