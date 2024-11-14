469e0387e334f53799efa885c8106b6453abb9f2
angelique <ademon63@hotmail.fr>
2024-11-14 15:51:02 +0100

    exercice 2

---
+++ Gestion-retaurant/exercice2/index.js
@@ -0,0 +1,14 @@
+let number = [8, 6, 4, 9];
+let tab = number.map((item) => item * 2);
+
+console.log(tab);
+
+let reservation = {
+    nomClient :"Connor",
+    numeroTable : 5,
+    nombrePersonnes : 4
+};
+const Myname ="Maxime";
+console.log(`Bonjour ${Myname}, la réservation pour ${reservation.nomClient} (${reservation.nombrePersonnes} personnes) est confirmée à la table <${reservation.nombrePersonnes}>.`);
+
+
